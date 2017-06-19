a574d46587acd42196900c07943adcb5bbc9bf7b
付琦 <m18210277823@163.com>
2017-06-19 17:43:57 +0800

    训练注释
    script_faster_rcnn_VOC2007_VGG16注释

--- matlab/experiments/+Dataset/voc2007_trainval.m
+++ matlab/experiments/+Dataset/voc2007_trainval.m
@@ -8,7 +8,9 @@ devkit                      = voc2007_devkit();
 
 switch usage
     case {'train'}
+	    %imdb_from_voc()函数:从VOC数据集中加载训练数据，如果是第一次运行该函数，结果会被保存到cache中，以后再运行这个程序时，不用重新计算，直接在cache里加载上次结果
         dataset.imdb_train    = {  imdb_from_voc(devkit, 'trainval', '2007', use_flip) };
+		%加载roi数据，提供了四种获取proposal的方法：with_selective_search、with_edge_box 、with_self_proposal以及RPN方法。如果第一次运行，则会计算初始的roi，并且将得到的roi保存到cache中，以后再运行程序时，直接从roi的cache中读取roi数据
         dataset.roidb_train   = cellfun(@(x) x.roidb_func(x), dataset.imdb_train, 'UniformOutput', false);
     case {'test'}
         dataset.imdb_test     = imdb_from_voc(devkit, 'trainval', '2007', use_flip) ;
--- matlab/experiments/script_faster_rcnn_VOC2007_VGG16.m
+++ matlab/experiments/script_faster_rcnn_VOC2007_VGG16.m
@@ -12,30 +12,34 @@ clear mex;
 clear is_valid_handle; % to clear init_key
 run(fullfile(fileparts(fileparts(mfilename('fullpath'))), 'startup'));
 %% -------------------- CONFIG --------------------
+%设置caffe版本
 opts.caffe_version          = 'caffe_faster_rcnn';
+%auto_select_gpu函数：查看电脑中有几个gpu，挨个检查free-memory，选择memory最大的gpu，返回其id
 opts.gpu_id                 = auto_select_gpu;
 active_caffe_mex(opts.gpu_id, opts.caffe_version);
 
-% do validation, or not 
+% do validation, or not；使用验证集 
 opts.do_val                 = true; 
-% model
+%VGG16_for_Faster_RCNN_VOC2007函数：加载平均图像、各个网络文件及训练时需要的模型（rpn1、rpn1的nms、fast1、fast1的nms、rpn2、rpn2的nms、fast2、fast2的nms），还设置了final test的nms
 model                       = Model.VGG16_for_Faster_RCNN_VOC2007;
 % cache base
 cache_base_proposal         = 'faster_rcnn_VOC2007_vgg_16layers';
 cache_base_fast_rcnn        = '';
 % train/test data
 dataset                     = [];
+%设置翻转，扩充数据集
 use_flipped                 = true;
+%voc2007_trainval()函数：获取训练数据，其中又调用了imdb_from_voc()函数，运行结果存到cache中
 dataset                     = Dataset.voc2007_trainval(dataset, 'train', use_flipped);
 dataset                     = Dataset.voc2007_test(dataset, 'test', false);
 
 %% -------------------- TRAIN --------------------
-% conf
+% conf；训练proposal时的配置，设置use_gpu、scales、max_size、fg_thresh等值，以及加载平均图像等
 conf_proposal               = proposal_config('image_means', model.mean_image, 'feat_stride', model.feat_stride);
 conf_fast_rcnn              = fast_rcnn_config('image_means', model.mean_image);
-% set cache folder for each stage
+% set cache folder for each stage；为每个阶段（训练RPN和fast rcnn）的训练保存cache
 model                       = Faster_RCNN_Train.set_cache_folder(cache_base_proposal, cache_base_fast_rcnn, model);
-% generate anchors and pre-calculate output size of rpn network 
+% generate anchors and pre-calculate output size of rpn network ；计算anchor和rpn网络的输出大小
 [conf_proposal.anchors, conf_proposal.output_width_map, conf_proposal.output_height_map] ...
                             = proposal_prepare_anchors(conf_proposal, model.stage1_rpn.cache_name, model.stage1_rpn.test_net_def_file);
 
@@ -80,7 +84,8 @@ opts.final_mAP              = Faster_RCNN_Train.do_fast_rcnn_test(conf_fast_rcnn
 % save final models, for outside tester
 Faster_RCNN_Train.gather_rpn_fast_rcnn_models(conf_proposal, conf_fast_rcnn, model, dataset);
 end
-
+%proposal_calc_output_size函数：计算rpn网络的输出大小，其中test_net_def_file就是网络test.prototxt
+%proposal_generate_anchors函数：计算anchor
 function [anchors, output_width_map, output_height_map] = proposal_prepare_anchors(conf, cache_name, test_net_def_file)
     [output_width_map, output_height_map] ...                           
                                 = proposal_calc_output_size(conf, test_net_def_file);
